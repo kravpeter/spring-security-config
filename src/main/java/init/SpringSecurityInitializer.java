aa30a00d64a6a2d124b701a534bbe11651a0ecd6
kravpeter <krav.peter@gmail.com>
2023-09-10 14:36:49 +0200

    sample project:
    UnsupportedOperationException in AbstractRequestMatcherRegistry#requestMatchers using programmatically defined ContextLoaderListener

---
+++ src/main/java/init/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/init/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package init;
+
+import config.SecurityConfig;
+import config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebConfig.class, SecurityConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
